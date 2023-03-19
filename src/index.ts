a6132a4f957b8267c66ff0f225e840a7eb0b33cb
Jacob Hands <jacob@gogit.io>
2023-03-19 12:01:25 -0500

    it works!

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { Env } from "./types"
 
 const upstream = 'https://cdnjs.cloudflare.com'
 
+interface KVMetadata {
+	headers: Record<string, string>
+}
+
 export default {
 	async fetch(
 		request: Request,
@@ -9,7 +13,45 @@ export default {
 		ctx: ExecutionContext
 	): Promise<Response> {
 		const url = new URL(request.url)
-		const kvRes = await env.KVCACHE.getWithMetadata()
-		return new Response("Hello World!")
+		if (!url.pathname.startsWith('/ajax/libs/')) {
+			return new Response('Not Found', { status: 404 })
+		}
+		const cache = caches.default
+		const cached = await cache.match(request)
+		if (cached) {
+			console.log('using edge cache')
+			return cached
+		}
+
+		const kvRes = await env.KVCACHE.getWithMetadata<KVMetadata>(url.pathname)
+		let response: Response
+		if (kvRes.value) {
+			const headers = new Headers({
+				'Cache-Control': 'public, max-age=30672000',
+				'Cache-Control-Allow-Origin': '*'
+			})
+			console.log('using kv cache')
+			if (kvRes.metadata) {
+				for (const [key, value] of Object.entries(kvRes.metadata.headers)) {
+					headers.set(key, value)
+				}
+			}
+			response = new Response(kvRes.value, { headers })
+		} else {
+			console.log('using upstream')
+			const upstreamRequest = new Request(upstream + url.pathname, request)
+			response = await fetch(upstreamRequest)
+			ctx.waitUntil(env.KVCACHE.put(url.pathname, await response.clone().arrayBuffer(), {
+				metadata: {
+					headers: {
+						'Cache-Control': response.headers.get('Cache-Control') || 'public, max-age=30672000',
+						'Cache-Control-Allow-Origin': '*'
+					}
+				},
+			}))
+		}
+
+		ctx.waitUntil(cache.put(request, response.clone()))
+		return response
 	},
 };
